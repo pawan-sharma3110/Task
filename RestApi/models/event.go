d2de35c68f80d7616ce39a29992d452b4d6db622
Pawan Sharma <pawansharma93520@gmail.com>
2024-08-12 01:57:11 +0530

    handler update no bugs left

--- RestApi/models/event.go
+++ RestApi/models/event.go
@@ -3,6 +3,7 @@ package models
 import (
 	"fmt"
 	"rest/api/database"
+	"time"
 )
 
 type Event struct {
@@ -10,28 +11,27 @@ type Event struct {
 	Name        string `binding:"required"`
 	Description string `binding:"required"`
 	Location    string `binding:"required"`
-	DateTime    string `binding:"required"`
+	DateTime    time.Time
 	UserID      int
 }
 
 var events = []Event{}
 
 func (e *Event) Save() error {
-	query := `INSERT INTO events(name,description,location,datetime,user_id) VALUES($1,$2,$3,$4,$5)`
+	query := `INSERT INTO events(name,description,location,datetime,user_id) VALUES($1,$2,$3,$4,$5)RETURNING id`
 	stmt, err := database.DB.Prepare(query)
 	if err != nil {
 		return fmt.Errorf("error preparing statement: %w", err)
 	}
 	defer stmt.Close()
-	_, err = stmt.Exec(e.Name, e.Description, e.Location, e.DateTime, e.UserID)
+	var id int
+	err = stmt.QueryRow(e.Name, e.Description, e.Location, time.Now(), e.UserID).Scan(&id)
 	if err != nil {
 		return fmt.Errorf("error executing statement: %w", err)
 	}
-	// id, err := result.LastInsertId()
-	// if err != nil {
-	// 	return fmt.Errorf("error while scan id %w", err)
-	// }
-	// e.ID = int(id)
+	e.ID = id
+	e.DateTime = time.Now()
+	fmt.Print(id)
 	return nil
 }
 
