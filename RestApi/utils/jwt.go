e1e6e10267dc7054298a35fed8500e881b5ea4cb
Pawan Sharma <pawansharma93520@gmail.com>
2024-08-15 22:49:41 +0530

    jwt verification

--- RestApi/utils/jwt.go
+++ RestApi/utils/jwt.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"errors"
+	"fmt"
 	"time"
 
 	"github.com/golang-jwt/jwt/v5"
@@ -13,4 +15,30 @@ func GernateToken(email string, userId int64) (string, error) {
 		"userId": userId,
 		"exp":    time.Now().Add(time.Hour * 2)})
 	return token.SignedString([]byte(secrectKey))
+}
+func VerifyToken(token string) error {
+	parsedToken, err := jwt.Parse(token, func(token *jwt.Token) (interface{}, error) {
+		_, ok := token.Method.(*jwt.SigningMethodHMAC)
+		if !ok {
+			return nil, errors.New("unexcepted sining methord")
+		}
+		return secrectKey, nil
+	})
+	if err != nil {
+		return errors.New("could not parsed token")
+	}
+
+	tokenIsValid := parsedToken.Valid
+
+	if !tokenIsValid {
+		return errors.New("invalid token")
+	}
+	claims, ok := parsedToken.Claims.(jwt.MapClaims)
+	if !ok {
+		return errors.New("invalid token claims")
+	}
+	email := claims["email"].(string)
+	userId := claims["userId"].(int64)
+	fmt.Println(email, userId)
+	return nil
 }
